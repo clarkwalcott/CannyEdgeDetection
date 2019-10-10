8f9533f193f3ed5627f33821df399d7c401bedeb
Clark Walcott <cgwy9f@mail.missouri.edu>
2019-10-10 12:14:42 -0500

    Finished filtered gradient step.

--- fgrad.py
+++ fgrad.py
@@ -1,5 +1,6 @@
 from cv2 import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Computes the filtered gradient on a given image
 # 1. Load an image
@@ -7,25 +8,25 @@ import numpy as np
 # 3. Find the x and y components of the gradient Fx and Fy at each point.
 # 4. Compute the edge strength F (magnitude of gradient) and edge orientation D = arctan(Fy/Fx) at each pixel
 # (Steps 2 and 3 may be combined by convolving the image with the derivative of a Gaussian)
+def gradient(img):
+    blur = GaussianBlur(img, (5,5), 0)
+    kernelx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]], np.float64)
+    kernely = np.array([[-1,-2,-1],[0,0,0],[1,2,1]], np.float64)
 
-
+    sobelx = filter2D(blur, CV_64F, kernel=kernelx)
+    sobely = filter2D(blur, CV_64F, kernel=kernely)
+    
+    F = np.hypot(sobelx, sobely, casting="same_kind")
+    D = np.arctan2(sobely, sobelx, casting="same_kind")
+    return F, D
 
 def main():
     img = imread("lena.png", 0)
-    
-    blur = GaussianBlur(img, (5,5), 0)
-    kernelx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]], np.float32)
-    kernely = np.array([[1,2,1],[0,0,0],[-1,-2,-1]], np.float32)
-    
-    sobelx = filter2D(blur, -1, kernelx)
-    sobely = filter2D(blur, -1, kernely)
-
-    F = np.hypot(sobelx, sobely)
-    F = F / F.max() * 255
-    D = np.arctan(sobely, sobelx)
-    print(F)
-    imshow('Gradient', F)
-    waitKey(0)
-    destroyAllWindows()
+    F, D = gradient(img)
+    D_Star = convertScaleAbs(D)
+    print(D_Star)
+    f = plt.figure(figsize=(20,20))
+    plt.imshow(F, cmap='gray')
+    plt.show()
 if __name__ == "__main__":
     main()
